684a741b95850ca34260ddd1f37b9030d1a87515
jiyeon037 <jiyeon7269@naver.com>
2019-05-12 22:47:05 +0900

    retrofit

--- app/src/main/java/com/eyesteam/mentormentee/LoginActivity.kt
+++ app/src/main/java/com/eyesteam/mentormentee/LoginActivity.kt
@@ -3,16 +3,46 @@ package com.eyesteam.mentormentee
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val userId = editID.text.toString()
+        val userPw = editPWD.text.toString()
+
         btnJoin.setOnClickListener {
             val nextIntent = Intent(this, JoinActivity::class.java)
             startActivity(nextIntent)
+        }
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl("http://15.164.95.90")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        // interface와 retrofit 결합
+        var server = retrofit.create(RetroService::class.java)
+
+        btnLogin.setOnClickListener {
+            server.postRequest(userId,userPw).enqueue(object : Callback<Void>{
+                override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                    println(response?.body().toString())
+                    Log.d("1111111111111", editID.text.toString())
+
+                }
+                override fun onFailure(call: Call<Void>, t: Throwable) {
+                   Log.d("222222222222","2222222222222")
+                }
+            })
         }
     }
 
--- app/src/main/java/com/eyesteam/mentormentee/RetroService.kt
+++ app/src/main/java/com/eyesteam/mentormentee/RetroService.kt
@@ -1,12 +1,18 @@
 package com.eyesteam.mentormentee
 
 import retrofit2.Call
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
+
+data class ResUserId(var UserId: String? = null)
+data class ResUserPw(var UserPw: String? = null)
 
 interface RetroService {
     @POST("/join.php")
-    fun postJoin(@Query("UserId") UserId : String, @Query("UserPW") UserPW : String, @Query("UserName") UserName : String,
-                 @Query("UserMail") UserMail : String) : Call<Void>
+    fun postJoin(@Query("join") join : Join) : Call<Void>
+
+    @FormUrlEncoded
+    @POST("/login.php")
+    fun postRequest(@Field("UserId")UserId: String,
+                    @Field("UserPw")UserPw: String): Call<Void>
 
 }
