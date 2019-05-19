2586934671c55c1a8a8f3923322cbbb93007ea07
jiyeon037 <jiyeon7269@naver.com>
2019-05-19 20:21:04 +0900

    retrofit

--- app/src/main/java/com/eyesteam/mentormentee/RetroService.kt
+++ app/src/main/java/com/eyesteam/mentormentee/RetroService.kt
@@ -1,18 +1,24 @@
 package com.eyesteam.mentormentee
 
+import com.eyesteam.mentormentee.models.LoginData
 import retrofit2.Call
 import retrofit2.http.*
 
-data class ResUserId(var UserId: String? = null)
-data class ResUserPw(var UserPw: String? = null)
+// data class ResponseDTO(var result: String? = null)
 
 interface RetroService {
     @POST("/join.php")
     fun postJoin(@Query("UserId") UserId: String, @Query("UserPW") UserPW: String, @Query("UserName") UserName : String, @Query("UserEmail") UserEmail : String) : Call<Void>
 
+    // This method is used fpr "POST"
     @FormUrlEncoded
     @POST("/login.php")
     fun postRequest(@Field("UserId")UserId: String,
-                    @Field("UserPw")UserPw: String): Call<Void>
+             @Field("UserPw")UserPw: String): Call<LoginData>
+
+    // This method is used for "GET"
+    @GET("/login.php")
+    fun getRequest(@Query("UserId")UserId: String,
+            @Query("UserPw")UserPw: String): Call<LoginData>
 
 }
---
+++ app/src/main/java/com/eyesteam/mentormentee/models/LoginData.kt
@@ -0,0 +1,3 @@
+package com.eyesteam.mentormentee.models
+
+data class LoginData(var UserId : String, var UserPw : String, var message : String, var responseCode : Int)
